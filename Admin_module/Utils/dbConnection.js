c2d488add0248403ea97289b27f29e9799054c06
SharviGabani <sharvigabani@gmail.com>
2022-05-12 13:47:59 +0530

    Db connection change

--- Admin_module/Utils/dbConnection.js
+++ Admin_module/Utils/dbConnection.js
@@ -1,9 +1,9 @@
 const {Sequelize} = require('sequelize')
 
-const sequelize = new Sequelize('dietsystem','root','sharvi3322', {
-      host: 'localhost',
+const sequelize = new Sequelize('dietsystemdb','admin','Detoxify2022', {
+      host: 'diet-system.cwt3nt3qxgnw.us-east-1.rds.amazonaws.com',
       dialect:'mysql',
-      // port: '1703'
+      port: '3306'
 })
 
 module.exports = sequelize
